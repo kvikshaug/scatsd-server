141589283009a42c31f355ea5bc47156afe9d200
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-04-22 20:02:08 +0200

    Run continuously until signalled to stop

--- src/main/scala/no/kvikshaug/statsd/StatsD.scala
+++ src/main/scala/no/kvikshaug/statsd/StatsD.scala
@@ -1,6 +1,19 @@
 package no.kvikshaug.statsd
 
-class StatsD {
+object StatsD extends Thread {
 
+  val sleepTime = 10 // seconds
+
+  var continue = true
+  Runtime.getRuntime().addShutdownHook(this)
+  override def run {
+    println("Caught signal; exiting.")
+    continue = false
+  }
+
+  def main(args: Array[String]) {
+    while(continue) {
+      Thread.sleep(sleepTime * 1000)
+    }
+  }
 }
-
