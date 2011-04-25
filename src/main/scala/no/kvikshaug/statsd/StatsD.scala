380b9a9d2fe095eb3c9a423bbda60c9810a8fe28
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-04-25 12:40:09 +0200

    Put loops in corresponding classes

--- src/main/scala/no/kvikshaug/statsd/StatsD.scala
+++ src/main/scala/no/kvikshaug/statsd/StatsD.scala
@@ -16,42 +16,60 @@ object StatsD extends Runnable {
     running = false
   }
 
-  val in = new Incoming
-  val out = new Outgoing
-  in.start
-  out.start
+  val inActor = new Incoming
+  val inThread = new Thread(inActor)
+  val outActor = new Outgoing
+  val outThread = new Thread(outActor)
 
   def main(args: Array[String]) {
-    val s = new DatagramSocket(8125)
-    val b: Array[Byte] = new Array(s.getReceiveBufferSize)
-    var d = new DatagramPacket(b, b.length)
-    println("Listening...")
+    // Start listening for incoming data
+    inActor.start
+    inThread.start
 
-    // Main loop
-    while(running) {
-      s.receive(d)
-      val str = new String(d.getData, 0, d.getLength)
-      in ! str
-    }
+    // Start sending data to graphite
+    outActor.start
+    outThread.start
   }
 
-  class Incoming extends Actor {
+  class Incoming extends Actor with Runnable {
+    def run {
+      val s = new DatagramSocket(8125)
+      val b: Array[Byte] = new Array(s.getReceiveBufferSize)
+      var d = new DatagramPacket(b, b.length)
+      println("Listening...")
+
+      while(running) {
+        s.receive(d)
+        val str = new String(d.getData, 0, d.getLength)
+        inActor ! str
+      }
+    }
+
     def act {
       loop {
         receive {
           case str: String =>
             // TODO handle string
-            println(str)
+            println("Receiving: " + str)
         }
       }
     }
   }
 
-  class Outgoing extends Actor {
+  class Outgoing extends Actor with Runnable {
+    def run {
+      while(running) {
+        Thread.sleep(sleepTime * 1000)
+      }
+    }
+
     def act {
       loop {
-        // TODO collect and send to graphite
-        Thread.sleep(sleepTime * 1000)
+        receive {
+          case str: String =>
+            // TODO send to graphite
+            println("Sending: " + str)
+        }
       }
     }
   }
