b9fccd96d1db5d4a91729720d6bb3c0cb0405f6b
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-04-26 21:00:22 +0200

    Parse the incoming metric into case class

--- src/main/scala/no/kvikshaug/statsd/StatsD.scala
+++ src/main/scala/no/kvikshaug/statsd/StatsD.scala
@@ -5,9 +5,33 @@ import java.net._
 import scala.actors.Actor
 import scala.actors.Actor._
 
+case class Metric(val name: String, val value: Double, val kind: String)
+
+object Parseable {
+  val validKinds = List("retain", "count", "time")
+
+  def unapply(str: String) = {
+    try {
+      val fields = str.split('|').toList
+      val name = fields(0)
+      val value = fields(1).toDouble
+      var kind = fields(2)
+      if(!validKinds.contains(kind)) {
+        throw new IllegalArgumentException("Unrecognized metric type: " + kind)
+      }
+      Some(Metric(name, value, kind))
+    } catch {
+      // TODO log instead of println
+      case e => println("Couldn't parse string '" + str + "' because: " + e.toString); None
+    }
+  }
+}
+
 object StatsD {
 
   val sleepTime = 10 // seconds
+
+  var metrics = List[Metric]()
 
   val inActor = new Incoming
   val inThread = new Thread(inActor)
@@ -40,9 +64,16 @@ object StatsD {
     def act {
       loop {
         receive {
-          case str: String =>
-            // TODO handle string
-            println("Receiving: " + str)
+          case Parseable(metric) =>
+            if(metrics.exists(_.name == metric.name)) {
+              // The metric exists, let's add to it based on what kind of metric it is
+              // TODO log
+            } else {
+              // The metric doesn't exist, add it to the list
+              // TODO log
+              metrics = metric :: metrics
+            }
+          case i => // TODO log
         }
       }
     }
