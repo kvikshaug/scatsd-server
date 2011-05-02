f5c279e2c84617146e5dd6ccc1fd81c73000fec4
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-05-02 18:30:58 +0200

    Save app state when shutdown, restore at restart

--- src/main/scala/no/kvikshaug/scatsd/ScatsD.scala
+++ src/main/scala/no/kvikshaug/scatsd/ScatsD.scala
@@ -1,5 +1,6 @@
 package no.kvikshaug.scatsd
 
+import java.io._
 import java.net._
 import java.util.Timer
 
@@ -18,8 +19,9 @@ object ScatsD {
   val inPort = (config \ "port").text.toInt
   val outHost = (config \ "graphite" \ "host").text
   val outPort = (config \ "graphite" \ "port").text.toInt
+  val stateFile = (config \ "stateFile").text
 
-  var metrics = List[Metric]()
+  var metrics: List[Metric] = Nil
 
   // true when a metric is being changed
   var busy = false
@@ -30,6 +32,7 @@ object ScatsD {
   val out = new Outgoing
 
   def main(args: Array[String]) {
+    metrics = loadState
     // Start the logger
     timer.scheduleAtFixedRate(Logger, 0, logCountInterval * 1000L)
 
@@ -40,5 +43,29 @@ object ScatsD {
     // Start sending data to graphite
     out.start
     timer.scheduleAtFixedRate(out, 0, flushInterval * 1000L)
+
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      override def run {
+        println(format("Received exit signal, saving state to %s...", stateFile))
+        saveState
+      }
+    });
+  }
+
+  def saveState {
+    val out = new ObjectOutputStream(new FileOutputStream(stateFile));
+    out.writeObject(metrics);
+    out.close();
+  }
+
+  def loadState: List[Metric] = {
+    if(!new File(stateFile).exists) {
+      return Nil
+    }
+    val in = new ObjectInputStream(new FileInputStream(stateFile))
+    val metrics = in.readObject.asInstanceOf[List[Metric]]
+    Logger.log(format("Pre-loaded %s metrics from %s", metrics.size, stateFile))
+    in.close()
+    return metrics
   }
 }
